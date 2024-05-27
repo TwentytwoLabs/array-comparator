f45902a3c693927ece601de1881cc3f3b44e13d1
Thibaut ALGRIN <algrin.thibaut@gmail.com>
2024-05-27 12:16:41 +0200

    Fix CI

--- src/Comparator/ArrayComparator.php
+++ src/Comparator/ArrayComparator.php
@@ -9,12 +9,12 @@ namespace TwentytwoLabs\ArrayComparator\Comparator;
  */
 class ArrayComparator implements ComparatorInterface
 {
-    public function support($expected): bool
+    public function support(mixed $expected): bool
     {
         return '<array>' === $expected;
     }
 
-    public function compare($expected, $value): void
+    public function compare(mixed $expected, mixed $value): void
     {
         if (true !== \is_array($value)) {
             throw new \Exception(sprintf('The value %s is not an array', $value));
